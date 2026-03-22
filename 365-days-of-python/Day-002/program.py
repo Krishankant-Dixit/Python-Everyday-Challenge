c58eee1df8ea593b452d560bc7dc7a43a21ac53d
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-03-22 12:28:58 +0530

    Day 002: How to use variables

--- 365-days-of-python/Day-002/program.py
+++ 365-days-of-python/Day-002/program.py
@@ -1,1 +1,27 @@
 # Day 2 Python Program
+     
+    ## This program demonstrates how to use variables in Python (by printing a name)...
+
+name = "Krishna"                        #String variable
+print("My name is", name)
+
+
+
+age = 20                                #Integer variable
+print("I am", age, "years old")
+
+
+
+height = 6.1                            #Float variable     
+print("My height is", height, "feet")
+
+
+
+is_student = True                       #Boolean variable
+print("I am a student:", is_student)
+
+
+
+#character variable (Python does not have a specific char type, so we use a string of length 1)
+char_var = "A"
+print("This character is", char_var)
