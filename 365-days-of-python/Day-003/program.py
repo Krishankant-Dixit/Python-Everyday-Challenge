e877efdf4955efa3ad5ecdc1e82a633aacd06870
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-03-23 11:37:42 +0530

    Day 002: User input & type conversion

--- 365-days-of-python/Day-003/program.py
+++ 365-days-of-python/Day-003/program.py
@@ -1,1 +1,11 @@
 # Day 3 Python Program
+
+    # User input
+name = input("Enter your name: ")       # this store the random string variable
+print("Hello", name)
+
+
+    # Type conversion
+age = int(input("Enter your age: "))     # this store the random string variable    
+print("You are", age, "years old")              # this will print the age of the user
+
