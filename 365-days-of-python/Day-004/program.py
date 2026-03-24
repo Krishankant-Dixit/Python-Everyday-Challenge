79b2a7dbecb7a5de424511216f9b5395b97e65e0
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-03-24 23:33:39 +0530

    Day 004: Adiition, substraction and some other operations

--- 365-days-of-python/Day-004/program.py
+++ 365-days-of-python/Day-004/program.py
@@ -1,1 +1,37 @@
 # Day 4 Python Program
+
+        #Addition
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+
+        #Subtraction
+a = int(input("Enter first number: "))          
+b = int(input("Enter second number: "))
+print("Difference =", a - b)
+
+        #Multiplication
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Product =", a * b)
+
+        #Division
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Quotient =", a / b)
+
+        #Modulus
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Remainder =", a % b)
+
+        #Exponentiation
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Power =", a ** b)
+
+        #Floor division
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Floor Division =", a // b)
+
