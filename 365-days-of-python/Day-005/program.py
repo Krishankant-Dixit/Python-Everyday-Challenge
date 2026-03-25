378fedcf08dda200a0f60929d1bd530722ad61d3
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-03-25 16:53:12 +0530

    Day 005: Simple Calculator

--- 365-days-of-python/Day-005/program.py
+++ 365-days-of-python/Day-005/program.py
@@ -1,1 +1,12 @@
 # Day 5 Python Program
+
+        # Simple Calculator
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("Add:", a+b)
+print("Sub:", a-b)
+print("Mul:", a*b)
+print("Div:", a/b)
+
