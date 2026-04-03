7d0981e15f517eca26d0778c9dc5170b9ba7527d
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-04-03 17:25:53 +0530

    Day 011: Multiplication Table

--- 365-days-of-python/Day-011/program.py
+++ 365-days-of-python/Day-011/program.py
@@ -1,1 +1,8 @@
 # Day 11 Python Program
+
+    # Multiplication Table
+number = int(input("Enter a number: "))
+
+print(f"Multiplication table for {number}:")
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
