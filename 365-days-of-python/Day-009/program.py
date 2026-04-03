a31ab88f6d3582c300231630ad3bfd5fc131f48c
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-04-03 17:18:59 +0530

    Day 009: Swap two numbers

--- 365-days-of-python/Day-009/program.py
+++ 365-days-of-python/Day-009/program.py
@@ -1,1 +1,10 @@
 # Day 9 Python Program
+
+    # Swap two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))    
+print(f"Before swapping: num1 = {num1}, num2 = {num2}")
+temp = num1
+num1 = num2
+num2 = temp
+print(f"After swapping: num1 = {num1}, num2 = {num2}")  
