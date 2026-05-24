6bea1c045fe51173d3cd219762d1ffb7d3b9b008
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-05-24 20:31:58 +0530

    Day 016: Palindrome Number

--- 365-days-of-python/Day-016/program.py
+++ 365-days-of-python/Day-016/program.py
@@ -1,1 +1,11 @@
 # Day 16 Python Program
+    # Palindrome Number
+num = int(input())
+temp = num
+rev = 0
+
+while num > 0:
+    rev = rev*10 + num%10
+    num //= 10
+
+print("Palindrome" if temp == rev else "Not Palindrome")
