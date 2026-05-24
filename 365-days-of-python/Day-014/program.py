ea0659f965d019a9c69fbb2b651363e2648c1867
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-05-24 20:22:53 +0530

    Day 014: Prime Number

--- 365-days-of-python/Day-014/program.py
+++ 365-days-of-python/Day-014/program.py
@@ -1,1 +1,11 @@
 # Day 14 Python Program
+    # Prime Number
+num = int(input())
+flag = True
+
+for i in range(2, num):
+    if num % i == 0:
+        flag = False
+        break
+
+print("Prime" if flag else "Not Prime")
