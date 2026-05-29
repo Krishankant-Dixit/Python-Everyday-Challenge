c36c96d33c4fc3e3637d1d8ce366bfed3f6afa3a
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-05-29 17:55:29 +0530

    Day 018: Armstrong Number

--- 365-days-of-python/Day-018/program.py
+++ 365-days-of-python/Day-018/program.py
@@ -1,1 +1,13 @@
 # Day 18 Python Program
+
+        # Armstrong Number
+num = int(input())
+temp = num
+sum_ = 0
+
+while num > 0:
+    digit = num % 10
+    sum_ += digit**3
+    num //= 10
+
+print("Armstrong" if temp == sum_ else "Not Armstrong")
