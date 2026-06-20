cc8722ed2d9f7a8ae52d18b6d555badc88ef7da2
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-06-20 19:23:00 +0530

    Day 030: Guess the number game

--- 365-days-of-python/Day-030/program.py
+++ 365-days-of-python/Day-030/program.py
@@ -1,1 +1,13 @@
 # Day 30 Python Program
+
+        #  Guess the number game
+import random
+
+num = random.randint(1, 10)
+
+guess = int(input("Guess number (1-10): "))
+
+if guess == num:
+    print("Correct!")
+else:
+    print("Wrong! Number was", num)
