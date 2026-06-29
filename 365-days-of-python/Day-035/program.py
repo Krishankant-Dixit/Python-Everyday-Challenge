e4cd41b6d3bc3c0940bf92449470133a3c56d122
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-06-29 16:15:40 +0530

    Day 035: Check Prime using Function

--- 365-days-of-python/Day-035/program.py
+++ 365-days-of-python/Day-035/program.py
@@ -1,1 +1,22 @@
 # Day 35 Python Program
+
+        # Check Prime using Function
+def is_prime(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+
+        if n % i == 0:
+            return False
+
+    return True
+
+
+num = int(input("Enter number: "))
+
+if is_prime(num):
+    print("Prime")
+else:
+    print("Not Prime")
