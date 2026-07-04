dab592a894b41d32c60a80849aaeaa4f21436002
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-05 01:28:21 +0530

    Day 036:  Factorial Function

--- 365-days-of-python/Day-036/program.py
+++ 365-days-of-python/Day-036/program.py
@@ -1,1 +1,13 @@
 # Day 36 Python Program
+
+        # Factorial Function
+def factorial(n):
+
+    fact = 1
+
+    for i in range(1, n+1):
+        fact *= i
+
+    return fact
+
+print(factorial(5))
