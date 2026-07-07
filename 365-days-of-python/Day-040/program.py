b125f290631c1cb2189b03f07c6eabd2c2f2d05f
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-07 22:13:29 +0530

    Day 040: Calculator using Functions

--- 365-days-of-python/Day-040/program.py
+++ 365-days-of-python/Day-040/program.py
@@ -1,1 +1,22 @@
 # Day 40 Python Program
+
+        # Calculator using Functions
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+a = int(input())
+b = int(input())
+
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
