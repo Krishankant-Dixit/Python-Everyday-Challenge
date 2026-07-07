0b937e2565c9d98ad1ffcb541fd44035c42ffa8c
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-07 13:50:59 +0530

    Day 039: Recursive Fibonacci

--- 365-days-of-python/Day-039/program.py
+++ 365-days-of-python/Day-039/program.py
@@ -1,1 +1,13 @@
 # Day 39 Python Program
+
+        # Recursive Fibonacci
+def fib(n):
+
+    if n <= 1:
+        return n
+
+    return fib(n-1) + fib(n-2)
+
+
+for i in range(10):
+    print(fib(i), end=" ")
