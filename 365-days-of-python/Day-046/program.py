32378c9475eaae026fa06d4869bdc11892a5f782
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-09 19:31:54 +0530

    Day 046: Count Upper & Lower Case

--- 365-days-of-python/Day-046/program.py
+++ 365-days-of-python/Day-046/program.py
@@ -1,1 +1,19 @@
 # Day 46 Python Program
+
+        # Count Upper & Lower Case
+s = input()
+
+upper = 0
+lower = 0
+
+for ch in s:
+
+    if ch.isupper():
+        upper += 1
+
+    elif ch.islower():
+        lower += 1
+
+
+print("Upper =", upper)
+print("Lower =", lower)
