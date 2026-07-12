b95495168bc7239d5d24b3733773236676a2df58
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-12 16:32:08 +0530

    Day 050: Password Generator

--- 365-days-of-python/Day-050/program.py
+++ 365-days-of-python/Day-050/program.py
@@ -1,1 +1,18 @@
 # Day 50 Python Program
+
+        # Password Generator
+import random
+import string
+
+chars = string.ascii_letters
+chars += string.digits
+chars += string.punctuation
+
+password = ""
+
+for i in range(10):
+
+    password += random.choice(chars)
+
+
+print(password)
