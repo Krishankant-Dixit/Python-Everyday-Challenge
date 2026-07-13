1914aad1e59d43d922c1ff2f26d834155de95924
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-13 20:35:50 +0530

    Day 053: Student Marks System

--- 365-days-of-python/Day-053/program.py
+++ 365-days-of-python/Day-053/program.py
@@ -1,1 +1,14 @@
 # Day 53 Python Program
+
+        # Student Marks System
+marks = {}
+
+for i in range(5):
+    subject = input("Enter Subject: ")
+    score = int(input("Enter Marks: "))
+    marks[subject] = score
+
+print("\nResult")
+
+for subject, score in marks.items():
+    print(subject, ":", score)
