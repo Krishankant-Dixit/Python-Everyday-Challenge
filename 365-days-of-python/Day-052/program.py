542174ae1c8722fad9a3d5218f856b606088eb29
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-13 20:32:15 +0530

    Day 052: Add / Update / Delete Dictionary

--- 365-days-of-python/Day-052/program.py
+++ 365-days-of-python/Day-052/program.py
@@ -1,1 +1,14 @@
 # Day 52 Python Program
+
+        # Add / Update / Delete Dictionary
+student = {
+    "name": "Krish",
+    "age": 20
+}
+
+student["course"] = "B.Tech"      # Add
+student["age"] = 21               # Update
+
+del student["name"]               # Delete
+
+print(student)
