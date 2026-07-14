4829c7625880162b52307ee94cc11daf33cc8737
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-14 20:31:10 +0530

    Day 055: Phone Book

--- 365-days-of-python/Day-055/program.py
+++ 365-days-of-python/Day-055/program.py
@@ -1,1 +1,13 @@
 # Day 55 Python Program
+
+        # Phone Book
+phonebook = {}
+
+for i in range(3):
+    name = input("Name: ")
+    number = input("Phone: ")
+    phonebook[name] = number
+
+search = input("Search Name: ")
+
+print(phonebook.get(search, "Contact Not Found"))
