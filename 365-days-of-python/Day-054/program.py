290c62811594c1585c58aaaa6e478d56003a3d3c
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-14 20:28:33 +0530

    Day 054: Word Frequency Counter

--- 365-days-of-python/Day-054/program.py
+++ 365-days-of-python/Day-054/program.py
@@ -1,1 +1,13 @@
 # Day 54 Python Program
+
+        # Word Frequency Counter
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+frequency = {}
+
+for word in words:
+    frequency[word] = frequency.get(word, 0) + 1
+
+print(frequency)
