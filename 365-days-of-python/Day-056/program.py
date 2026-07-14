0e0654087f771ff088d6c479b0f2b4c82e8dba4a
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-14 20:32:42 +0530

    Day 056: Inventory Management System

--- 365-days-of-python/Day-056/program.py
+++ 365-days-of-python/Day-056/program.py
@@ -1,1 +1,11 @@
 # Day 56 Python Program
+
+        # Inventory Management System
+inventory = {
+    "Pen": 20,
+    "Book": 15,
+    "Notebook": 30
+}
+
+for item, quantity in inventory.items():
+    print(item, ":", quantity)
