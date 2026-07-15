c01ebc9c5a98fa21d3bf394baab7eb1ee28fe686
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-15 16:17:24 +0530

    Day 058: Nested Dictonary

--- 365-days-of-python/Day-058/program.py
+++ 365-days-of-python/Day-058/program.py
@@ -1,1 +1,16 @@
 # Day 58 Python Program
+
+        # Nested Dictonary
+students = {
+    "101": {
+        "name": "Krish",
+        "age": 20
+    },
+
+    "102": {
+        "name": "Rahul",
+        "age": 21
+    }
+}
+
+print(students)
