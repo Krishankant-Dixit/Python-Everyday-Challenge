31609179d445b4b70bb796026d01e6db4d3700c8
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-15 16:18:34 +0530

    Day 059: Menu Driven Dictionary App

--- 365-days-of-python/Day-059/program.py
+++ 365-days-of-python/Day-059/program.py
@@ -1,1 +1,23 @@
 # Day 59 Python Program
+
+        # Menu Driven Dictionary App
+student = {}
+
+while True:
+
+    print("\n1. Add")
+    print("2. View")
+    print("3. Exit")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 1:
+        key = input("Key: ")
+        value = input("Value: ")
+        student[key] = value
+
+    elif choice == 2:
+        print(student)
+
+    elif choice == 3:
+        break
