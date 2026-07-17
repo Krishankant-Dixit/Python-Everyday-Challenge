5b8234874488f4b344dd64779f7fb68cc6adc5cb
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-17 14:46:06 +0530

    Day 063: Append File

--- 365-days-of-python/Day-063/program.py
+++ 365-days-of-python/Day-063/program.py
@@ -1,1 +1,10 @@
 # Day 63 Python Program
+
+        # Append File
+file = open("sample.txt", "a")
+
+file.write("\nThis line is added later.")
+
+file.close()
+
+print("Data Appended Successfully.")
