ccdcf62f30524fc4cd9d6b51fbda4f027cf6c300
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-17 14:36:18 +0530

    Day 061: Create File

--- 365-days-of-python/Day-061/program.py
+++ 365-days-of-python/Day-061/program.py
@@ -1,1 +1,10 @@
 # Day 61 Python Program
+
+        # Create File
+file = open("sample.txt", "w")
+
+file.write("Hello, Welcome to Python File Handling!")
+
+file.close()
+
+print("File Created Successfully.")
