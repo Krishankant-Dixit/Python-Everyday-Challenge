5568045f1d96cf49bdd3442f474e237e0c66a7ba
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-17 14:32:31 +0530

    Day 060: Contact Book Project

--- 365-days-of-python/Day-060/program.py
+++ 365-days-of-python/Day-060/program.py
@@ -1,1 +1,34 @@
 # Day 60 Python Program
+
+        # Contact Book Project
+contacts = {}
+
+while True:
+
+    print("\n1. Add Contact")
+    print("2. Search Contact")
+    print("3. View All")
+    print("4. Exit")
+
+    choice = int(input("Choice: "))
+
+    if choice == 1:
+
+        name = input("Name: ")
+        phone = input("Phone: ")
+
+        contacts[name] = phone
+
+    elif choice == 2:
+
+        name = input("Search Name: ")
+
+        print(contacts.get(name, "Not Found"))
+
+    elif choice == 3:
+
+        print(contacts)
+
+    elif choice == 4:
+
+        break
