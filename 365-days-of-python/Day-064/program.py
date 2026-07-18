3518fec87bf71d38f3e24c6a83440fedfa96347a
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-18 19:22:51 +0530

    Day 064: Count words in a File

--- 365-days-of-python/Day-064/program.py
+++ 365-days-of-python/Day-064/program.py
@@ -1,1 +1,12 @@
 # Day 64 Python Program
+
+        # Count words in a File
+file = open("sample.txt", "r")
+
+data = file.read()
+
+words = data.split()
+
+print("Total Words:", len(words))
+
+file.close()
