9d36eb369552867da5070be1ebcf3782a547e2f6
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-18 19:25:14 +0530

    Day 066: Copy File

--- 365-days-of-python/Day-066/program.py
+++ 365-days-of-python/Day-066/program.py
@@ -1,1 +1,16 @@
 # Day 66 Python Program
+
+        # Copy File
+source = open("sample.txt", "r")
+
+data = source.read()
+
+source.close()
+
+destination = open("copy.txt", "w")
+
+destination.write(data)
+
+destination.close()
+
+print("File Copied Successfully.")
