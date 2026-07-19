881d41dfffc72aa38b6a2e53527940ab0e083b67
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-19 17:13:00 +0530

    Day 069: Student Record System

--- 365-days-of-python/Day-069/program.py
+++ 365-days-of-python/Day-069/program.py
@@ -1,1 +1,13 @@
 # Day 69 Python Program
+
+        # Student Record System
+file = open("students.txt", "a")
+
+name = input("Enter Name: ")
+marks = input("Enter Marks: ")
+
+file.write(name + " : " + marks + "\n")
+
+file.close()
+
+print("Record Saved Successfully.")
