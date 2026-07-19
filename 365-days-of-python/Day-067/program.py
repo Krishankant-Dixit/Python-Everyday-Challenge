68120dd6cda66558b82b920074aadec4de8ca265
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-19 17:08:14 +0530

    Day 067: Search word

--- 365-days-of-python/Day-067/program.py
+++ 365-days-of-python/Day-067/program.py
@@ -1,1 +1,15 @@
 # Day 67 Python Program
+
+        # Search word
+file = open("sample.txt", "r")
+
+data = file.read()
+
+word = input("Enter word to search: ")
+
+if word in data:
+    print("Word Found")
+else:
+    print("Word Not Found")
+
+file.close()
