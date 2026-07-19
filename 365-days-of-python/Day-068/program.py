7dc940cbd7ba50c5dbcb5688f7bc9db6ca236243
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-19 17:12:02 +0530

    Day 068: Replace word in file

--- 365-days-of-python/Day-068/program.py
+++ 365-days-of-python/Day-068/program.py
@@ -1,1 +1,21 @@
 # Day 68 Python Program
+
+        # Replace word in file
+file = open("sample.txt", "r")
+
+data = file.read()
+
+old_word = input("Enter word to replace: ")
+new_word = input("Enter new word: ")
+
+data = data.replace(old_word, new_word)
+
+file.close()
+
+file = open("sample.txt", "w")
+
+file.write(data)
+
+file.close()
+
+print("Word Replaced Successfully.")
