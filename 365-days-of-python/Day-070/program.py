3d3d8126f6019accc90da92ceed178af0fb668f3
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-20 23:11:04 +0530

    Day 070: Notes Manager Project

--- 365-days-of-python/Day-070/program.py
+++ 365-days-of-python/Day-070/program.py
@@ -1,1 +1,34 @@
 # Day 70 Python Program
+
+        # Notes Manager Project
+while True:
+
+    print("\n1. Add Note")
+    print("2. View Notes")
+    print("3. Exit")
+
+    choice = int(input("Enter Choice: "))
+
+    if choice == 1:
+
+        note = input("Write Note: ")
+
+        file = open("notes.txt", "a")
+
+        file.write(note + "\n")
+
+        file.close()
+
+    elif choice == 2:
+
+        file = open("notes.txt", "r")
+
+        print("\nYour Notes:\n")
+
+        print(file.read())
+
+        file.close()
+
+    elif choice == 3:
+
+        break
