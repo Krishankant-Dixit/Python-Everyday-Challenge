35bf7601401deb670448816a3c6325266c0f5fe5
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-22 23:49:59 +0530

    Day 073: finally block example

--- 365-days-of-python/Day-073/program.py
+++ 365-days-of-python/Day-073/program.py
@@ -1,1 +1,12 @@
 # Day 73 Python Program
+
+        # finally
+try:
+    num = int(input("Enter number: "))
+    print(num)
+
+except:
+    print("Error occurred")
+
+finally:
+    print("Program finished")
