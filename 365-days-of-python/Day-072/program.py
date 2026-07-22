5a64b168da01d59c52de1f30a7361bb6f87d8cd6
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-22 23:38:13 +0530

    Day 072: Handle multiple exceptions

--- 365-days-of-python/Day-072/program.py
+++ 365-days-of-python/Day-072/program.py
@@ -1,1 +1,14 @@
 # Day 72 Python Program
+
+        # Multiple Exceptions
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    print(a / b)
+
+except ValueError:
+    print("Please enter numbers only.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
