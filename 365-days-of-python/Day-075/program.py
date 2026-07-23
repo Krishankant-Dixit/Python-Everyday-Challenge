dc649463d60468219d64be3a129637b487a7285e
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-23 21:45:55 +0530

    Day 75: Custom exception example

--- 365-days-of-python/Day-075/program.py
+++ 365-days-of-python/Day-075/program.py
@@ -1,1 +1,12 @@
 # Day 75 Python Program
+
+        # Custom Exception
+class AgeError(Exception):
+    pass
+
+age = int(input("Enter age: "))
+
+if age < 18:
+    raise AgeError("Age must be 18 or above")
+
+print("Eligible")
