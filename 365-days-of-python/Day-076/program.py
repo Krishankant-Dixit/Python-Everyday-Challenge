28721e915b64d362a00d7470a5041f958dedf313
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-24 20:01:20 +0530

    Day 76: Divide calculator with exception handling

--- 365-days-of-python/Day-076/program.py
+++ 365-days-of-python/Day-076/program.py
@@ -1,1 +1,16 @@
 # Day 76 Python Program
+
+        # Divide Calculator
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    result = a / b
+
+    print("Result =", result)
+
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+
+except ValueError:
+    print("Enter valid numbers")
