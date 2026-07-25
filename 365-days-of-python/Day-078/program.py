762123c0a8b6c517ce6d04619a086f8f0d13ec47
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-25 23:33:58 +0530

    Day 78: Input validation using try-except

--- 365-days-of-python/Day-078/program.py
+++ 365-days-of-python/Day-078/program.py
@@ -1,1 +1,12 @@
 # Day 78 Python Program
+
+        # Input Validation
+while True:
+
+    try:
+        num = int(input("Enter a number: "))
+        print("Valid input:", num)
+        break
+
+    except ValueError:
+        print("Please enter a valid integer")
