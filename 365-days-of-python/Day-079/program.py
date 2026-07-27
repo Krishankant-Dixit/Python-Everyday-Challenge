b36307efe582c2650a2c7f1948f4be5e8ec11e0c
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-27 23:44:57 +0530

    Day 79: ATM simulation with exception handling

--- 365-days-of-python/Day-079/program.py
+++ 365-days-of-python/Day-079/program.py
@@ -1,1 +1,18 @@
 # Day 79 Python Program
+
+        # ATM Simulation
+balance = 5000
+
+try:
+    amount = int(input("Enter withdrawal amount: "))
+
+    if amount > balance:
+        print("Insufficient balance")
+
+    else:
+        balance -= amount
+        print("Withdraw successful")
+        print("Remaining balance:", balance)
+
+except ValueError:
+    print("Enter valid amount")
