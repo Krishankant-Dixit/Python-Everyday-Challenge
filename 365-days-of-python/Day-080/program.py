46b9ab98140329d677c7abb4b25091bb38a315b1
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-30 21:45:13 +0530

    Day 80: Login system with error handling

--- 365-days-of-python/Day-080/program.py
+++ 365-days-of-python/Day-080/program.py
@@ -1,1 +1,18 @@
 # Day 80 Python Program
+
+        # Login System
+username = "admin"
+password = "1234"
+
+try:
+    u = input("Enter username: ")
+    p = input("Enter password: ")
+
+    if u == username and p == password:
+        print("Login successful")
+
+    else:
+        print("Invalid username or password")
+
+except Exception as e:
+    print("Error:", e)
