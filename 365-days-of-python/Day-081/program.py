1ced498609bff9b1cfeb538b59a4769d3a9e02f5
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-31 22:26:55 +0530

    Day 81: Learn math module

--- 365-days-of-python/Day-081/program.py
+++ 365-days-of-python/Day-081/program.py
@@ -1,1 +1,13 @@
 # Day 81 Python Program
+
+        # Math Module
+import math
+
+num = int(input("Enter a number: "))
+
+print("Square Root:", math.sqrt(num))
+print("Power:", math.pow(num, 2))
+print("Factorial:", math.factorial(num))
+print("Ceil:", math.ceil(5.2))
+print("Floor:", math.floor(5.8))
+print("Pi:", math.pi)
