918f74d67a3f27c865ea5eef1828f862fcc34956
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-01 16:49:28 +0530

    Day 82: Learn random module

--- 365-days-of-python/Day-082/program.py
+++ 365-days-of-python/Day-082/program.py
@@ -1,1 +1,11 @@
 # Day 82 Python Program
+
+        # random Module
+import random
+
+print("Random Integer:", random.randint(1, 100))
+print("Random Float:", random.random())
+
+colors = ["Red", "Blue", "Green", "Black"]
+
+print("Random Choice:", random.choice(colors))
