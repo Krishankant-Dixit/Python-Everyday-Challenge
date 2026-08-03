9594ec0fd71f7c97ae7eaf991daa3e6837dce2ad
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-03 12:16:54 +0530

    Day 83: Learn datetime module

--- 365-days-of-python/Day-083/program.py
+++ 365-days-of-python/Day-083/program.py
@@ -1,1 +1,12 @@
 # Day 83 Python Program
+
+        # datetime Module
+from datetime import datetime
+
+now = datetime.now()
+
+print("Current Date & Time:", now)
+print("Year:", now.year)
+print("Month:", now.month)
+print("Day:", now.day)
+print("Time:", now.strftime("%H:%M:%S"))
