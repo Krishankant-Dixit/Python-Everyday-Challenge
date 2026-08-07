1d428d893f259183661ea052bc7d1a8484581976
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-07 14:53:25 +0530

    Day 88: Import custom module

--- 365-days-of-python/Day-088/program.py
+++ 365-days-of-python/Day-088/program.py
@@ -1,1 +1,16 @@
 # Day 88 Python Program
+
+        # Import Custom Module
+# calculator.py
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+
+# main.py
+import calculator
+
+print(calculator.add(10, 20))
+print(calculator.sub(20, 10))
