f3cf4eecd46fb3fca54868d223871daf47eea1a9
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-09 13:52:49 +0530

    Day 89: Create Python package

--- 365-days-of-python/Day-089/program.py
+++ 365-days-of-python/Day-089/program.py
@@ -1,1 +1,12 @@
 # Day 89 Python Program
+
+        # Create Package
+
+# calculator.py
+def multiply(a, b):
+    return a * b
+
+
+# main.py
+from mypackage import calculator
+print(calculator.multiply(5, 6))
