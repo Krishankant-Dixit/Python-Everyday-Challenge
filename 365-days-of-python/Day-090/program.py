5997e83abfe1ffe8b8c9cdd6a030d1ce07a679b9
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-09 13:56:01 +0530

    Day 90: Build utility package

--- 365-days-of-python/Day-090/program.py
+++ 365-days-of-python/Day-090/program.py
@@ -1,1 +1,20 @@
 # Day 90 Python Program
+
+        # Utility Package
+
+# math_utils.py
+def square(n):
+    return n * n
+
+
+# string_utils.py
+def reverse(text):
+    return text[::-1]
+
+
+# main.py
+from utils.math_utils import square
+from utils.string_utils import reverse
+
+print(square(10))
+print(reverse("Python"))
