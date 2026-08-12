1b603b2de112850fe6fa8a157b92d9ce2a09babd
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-12 16:35:03 +0530

    Day 92: Add email validation

--- 365-days-of-python/Day-092/program.py
+++ 365-days-of-python/Day-092/program.py
@@ -1,1 +1,13 @@
 # Day 92 Python Program
+
+        # Email Validation
+import re
+
+email = input("Enter email: ")
+
+pattern = r"^[\w.-]+@[\w.-]+\.\w+$"
+
+if re.match(pattern, email):
+    print("Valid Email")
+else:
+    print("Invalid Email")
