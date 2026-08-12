53070539345899127449053fb85d19a561cc7cf2
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-12 16:37:48 +0530

    Day 93: Add password validation

--- 365-days-of-python/Day-093/program.py
+++ 365-days-of-python/Day-093/program.py
@@ -1,1 +1,11 @@
 # Day 93 Python Program
+
+        # Password Validation
+import re
+
+password = input("Enter password: ")
+
+if len(password) >= 8 and re.search(r"[A-Z]", password) and re.search(r"[0-9]", password):
+    print("Valid Password")
+else:
+    print("Password must have 8+ characters, one uppercase letter and one number")
