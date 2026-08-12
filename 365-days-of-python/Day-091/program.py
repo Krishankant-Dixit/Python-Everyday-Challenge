7d2f23f3dd3977e0f714635256520d6df2e3c36c
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-12 16:33:22 +0530

    Day 91: Learn regex basics

--- 365-days-of-python/Day-091/program.py
+++ 365-days-of-python/Day-091/program.py
@@ -1,1 +1,11 @@
 # Day 91 Python Program
+
+        # Regex Basics
+import re
+
+text = "Python is powerful and Python is easy."
+
+matches = re.findall("Python", text)
+
+print("Matches:", matches)
+print("Count:", len(matches))
