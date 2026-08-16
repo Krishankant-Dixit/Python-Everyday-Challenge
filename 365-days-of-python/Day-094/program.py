53db0d88133843f514cb6eb8d304d50a8233d938
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-17 00:55:05 +0530

    Day 94: Build text analyzer

--- 365-days-of-python/Day-094/program.py
+++ 365-days-of-python/Day-094/program.py
@@ -1,1 +1,12 @@
 # Day 94 Python Program
+
+        # Text Analyzer
+text = input("Enter text: ")
+
+words = text.split()
+characters = len(text)
+spaces = text.count(" ")
+
+print("Characters:", characters)
+print("Words:", len(words))
+print("Spaces:", spaces)
