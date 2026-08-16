d98dc9f252aca142512a2a91ab386ae79f18240b
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-17 01:04:20 +0530

    Day 96: Implement text encryption

--- 365-days-of-python/Day-096/program.py
+++ 365-days-of-python/Day-096/program.py
@@ -1,1 +1,11 @@
 # Day 96 Python Program
+
+        # Encryption
+text = input("Enter text: ")
+
+encrypted = ""
+
+for char in text:
+    encrypted += chr(ord(char) + 3)
+
+print("Encrypted:", encrypted)
