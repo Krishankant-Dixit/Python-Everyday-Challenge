61d4b8c08c3a573654532d06d8e6a62c248a1d5c
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-17 00:56:37 +0530

    Day 95: Count character frequency

--- 365-days-of-python/Day-095/program.py
+++ 365-days-of-python/Day-095/program.py
@@ -1,1 +1,13 @@
 # Day 95 Python Program
+
+        # Character Frequency
+text = input("Enter text: ")
+
+frequency = {}
+
+for char in text:
+    if char != " ":
+        frequency[char] = frequency.get(char, 0) + 1
+
+for char, count in frequency.items():
+    print(char, ":", count)
