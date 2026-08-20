ee7d0e012bce100332680137bdf8def0ca60f740
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-20 20:07:08 +0530

    Day 97: Implement text decryption

--- 365-days-of-python/Day-097/program.py
+++ 365-days-of-python/Day-097/program.py
@@ -1,1 +1,11 @@
 # Day 97 Python Program
+
+        # Decryption
+text = input("Enter encrypted text: ")
+
+decrypted = ""
+
+for char in text:
+    decrypted += chr(ord(char) - 3)
+
+print("Decrypted:", decrypted)
