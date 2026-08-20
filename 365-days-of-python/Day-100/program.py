c02aff736967864f18cbb1a3a1f506a29fad2b95
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-20 20:11:35 +0530

    Day 100: Build text statistics analyzer

--- 365-days-of-python/Day-100/program.py
+++ 365-days-of-python/Day-100/program.py
@@ -1,1 +1,33 @@
 # Day 100 Python Program
+
+        # Text Statistics
+text = input("Enter text: ")
+
+words = text.split()
+
+vowels = 0
+consonants = 0
+digits = 0
+spaces = 0
+
+for char in text:
+
+    if char.lower() in "aeiou":
+        vowels += 1
+
+    elif char.isalpha():
+        consonants += 1
+
+    elif char.isdigit():
+        digits += 1
+
+    elif char.isspace():
+        spaces += 1
+
+
+print("Characters:", len(text))
+print("Words:", len(words))
+print("Vowels:", vowels)
+print("Consonants:", consonants)
+print("Digits:", digits)
+print("Spaces:", spaces)
