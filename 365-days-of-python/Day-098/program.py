00b3aeb64d33793db139a96e7e80cd35f7f4362f
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-20 20:09:08 +0530

    Day 98: Implement Caesar cipher

--- 365-days-of-python/Day-098/program.py
+++ 365-days-of-python/Day-098/program.py
@@ -1,1 +1,18 @@
 # Day 98 Python Program
+
+        # Caesar Cipher
+text = input("Enter text: ")
+shift = 3
+
+result = ""
+
+for char in text:
+
+    if char.isalpha():
+        base = ord("A") if char.isupper() else ord("a")
+        result += chr((ord(char) - base + shift) % 26 + base)
+
+    else:
+        result += char
+
+print("Encrypted:", result)
