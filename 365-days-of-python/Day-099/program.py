0f7b96aeade8f4dd40f08f5278467a2defe96e08
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-08-20 20:10:25 +0530

    Day 99: Practice string formatting

--- 365-days-of-python/Day-099/program.py
+++ 365-days-of-python/Day-099/program.py
@@ -1,1 +1,10 @@
 # Day 99 Python Program
+
+        # String Formatter
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+course = input("Enter course: ")
+
+message = f"My name is {name}. I am {age} years old and studying {course}."
+
+print(message)
